b0fda056b41037aa579a709ea80f2f8e0596f3ec
ncarnahan19 <ncarnahan123@gmail.com>
2021-10-19 18:33:00 -0400

    dddddfa;lsdkjf needs to be better first commit# Please enter the commit message for your changes. Lines starting

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <Header title={1}/>
+
+    </div>
+  );
+}
+
+export default App;
