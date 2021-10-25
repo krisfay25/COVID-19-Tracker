c00270432d772a7e484077038a94b4f115327ccc
kc <kaelachen22@gmail.com>
2021-10-25 12:37:06 -0400

    Structure + first route, total vaccinated per county

--- api/app.py
+++ api/app.py
@@ -1,38 +1,48 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 from flask_cors import CORS
 import mysql.connector
 
+# Initialize flask app
 app = Flask(__name__)
 CORS(app)
 
-def test():
-    #Establish parameters for connection to database
-    config = {
+# Establish parameters for connection to database
+config = {
     'user': '', # USE YOUR USERNAME, NEVER PUSH THIS
     'password': '', # USE YOUR PASSWORD, NEVER PUSH THIS
     'host': 'localhost',
     'port': 3306,
     'database': 'RI_DATA',
-    'raise_on_warnings': True                    
-    }
+    'raise_on_warnings': True                     
+}
 
-    #Establish connection to database
-    db_connection = mysql.connector.connect(**config)
+# Connect to the database when needed
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = mysql.connector.connect(**config)
+    return db
 
-    #Establishes a cursor
-    cur = db_connection.cursor()
-    
-    query = "SELECT * FROM Municipality"
-        
-    # Execute query and commit to database
-    cur.execute(query)
+# Close database after request
+@app.teardown_request
+def close_database(error):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# Get the total number vaccinated
+@app.route('/<county>/total-vaccinated', methods=['GET'])
+def total_vaccinated(county):
+    # get the totals from all municipalities in the county
+    cur = get_db().cursor()
+    cur.execute(f'SELECT total_vaccinated FROM Municipality WHERE County="{county} County"')
     result = cur.fetchall()
-    print("result:")
-    print(result)
-    #db_connection.commit()
-    
-    # Closes database connection and closes cursor
-    cur.close()
-    db_connection.close()
 
-test()
+    # add up the totals
+    total = 0
+    for element in result:
+        total += element[0]
+
+    cur.close()
+
+    return jsonify(total)
