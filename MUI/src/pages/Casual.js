63a0b0456e03a2040413e71cf1779236d52d1b1e
krisfay25 <blackninja675@gmail.com>
2021-10-25 05:02:53 -0400

    Created map
    Created map and set map to Rhode Island. Currently working on geoJSON to draw the county boundaries.

--- MUI/src/pages/Casual.js
+++ MUI/src/pages/Casual.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import PropTypes from 'prop-types'
+import './leaflet/Casual.css';
+import './leaflet/counties_geoJSON.js';
 
 function Casual(props) {
-    return (
-        <div>
-          <h1>Casual Page</h1> 
-        </div>
-    )
+  return (
+    <>
+      <MapContainer center={[41.700, -71.500]} zoom={9} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[41.700, -71.500]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+
+      </MapContainer>
+      </>
+  );
 }
 
-Casual.propTypes = {
-
-}
 
 export default Casual;
 
