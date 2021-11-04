04fbcbf6825a0675e2383abdf75b8a573a491711
krisfay25 <blackninja675@gmail.com>
2021-11-04 00:04:53 -0400

    Update Casual.js

--- MUI/src/pages/Casual.js
+++ MUI/src/pages/Casual.js
@@ -5,7 +5,22 @@ import './leaflet/counties_geoJSON.js';
 import TemporaryDrawer from '../components/TemporaryDrawer';
 
 
-      
+//Getting Data
+const  getData = () => {
+  fetch('http://localhost:5000/all/geo_json_data', {
+      headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      }
+  }).then(response=>response.json())
+  .then(data => {
+      console.log(data)
+      setMain(data);
+  });
+}
+useEffect(() => {
+  getData()
+},[])
 
 function Casual(props) {
     return (
@@ -16,6 +31,8 @@ function Casual(props) {
            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
+          <Polygon positions={coords}/>
           </MapContainer>
         </div>
       
