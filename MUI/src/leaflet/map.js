9c5872a2f746402915297fa3dedbdccd8c77126e
krisfay25 <blackninja675@gmail.com>
2021-11-06 02:20:06 -0400

    Counties Highlighted
    Deleted unecessary files and fixed map and put highlighted counties

--- MUI/src/leaflet/map.js
+++
@@ -1,5 +0,0 @@
-import React from 'React';
-
-var mymap = L.map('mapid').setView([41.700,  -71.500], 13);
-
-export default Casual;
--- MUI/src/pages/Casual.js
+++ MUI/src/pages/Casual.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import React, { useEffect, useState, useRef } from 'react'
+import { MapContainer, TileLayer, Polygon, GeoJSON } from 'react-leaflet'
 import './leaflet/Casual.css';
-import './leaflet/counties_geoJSON.js';
 import TemporaryDrawer from '../components/TemporaryDrawer';
+import axios from 'axios';
 
+//API call to retrive geoJSON data
+const RetrieveData = () => {
+  const [data, setData] = React.useState();
 
-//Getting Data
-const  getData = () => {
-  fetch('http://localhost:5000/all/geo_json_data', {
-      headers : { 
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-      }
-  }).then(response=>response.json())
-  .then(data => {
-      console.log(data)
-      setMain(data);
-  });
-}
-useEffect(() => {
-  getData()
-},[])
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get("http://localhost:5000/all/geo_json_data");
+      setData(response.data);
+    };
+    getData();
+  },[]);
+
+  if (data) {
+    return <GeoJSON data={data} />;
+  } else {
+      return null;
+    }
+};
 
 function Casual(props) {
-    return (
-        <div>
-          <MapContainer center={[41.700, -71.500]} zoom={10} scrollWheelZoom={true}>
-          <TemporaryDrawer/>
-          <TileLayer
-           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+  return (
 
-          <Polygon positions={coords}/>
-          </MapContainer>
-        </div>
-      
+    <div>
+      <MapContainer center={[41.700, -71.500]} zoom={10} scrollWheelZoom={true}>
+      <TemporaryDrawer/>
+      <TileLayer
+       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <RetrieveData/>
+      </MapContainer>
+    </div>
+
   );
-}
-
+};
 
 export default Casual;
 
