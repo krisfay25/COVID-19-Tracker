b5979b7f5fa359b4ae81e395acceafc5cd8c8606
kc <kaelachen22@gmail.com>
2021-11-12 15:53:13 -0500

    info view expandable to more counties

--- MUI/src/pages/Informative.js
+++ MUI/src/pages/Informative.js
@@ -10,67 +10,39 @@ import Paper from '@mui/material/Paper';
 
 export const Informative = () => {
     const [loading, setLoading] = useState(false);
-    const [main] = useState([]);
+    const [main, setMain] = useState([]);
+    // Fips codes for counties we currently support
+    const [counties] = useState([44001, 44003, 44005, 44009]);
+
+    // The columns of the table and what property in the data they correspond to
+    const columns = [
+        { name: 'Fips', property: 'fips' },
+        { name: 'State', property: 'state' },
+        { name: 'County', property: 'county_name' },
+        { name: 'Total Cases', property: 'total_cases' },
+        { name: 'Total Deaths', property: 'total_deaths' },
+        { name: 'Total Hospitalizations', property: 'total_hospital' },
+        { name: 'Total Vaccinations', property: 'total_vaccinated' },
+        { name: 'Last Updated', property: 'last_updated_timestamp' }
+    ];
 
     useEffect(() => {
-            //fips 44001
-            fetch('http://localhost:5000/county/44001', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
+        // Load county data
+        Promise.all(counties.map(fips =>
+            fetch(`http://localhost:5000/county/${fips}`)
+                .then(response => response.json())
+        )).then(data => {
+            // Replace state id with state name
+            for (let county of data) {
+                if (county.state_id === 44) {
+                    county.state = 'Rhode Island';
                 }
-            }).then(response=>response.json())
-            .then(data => {
-                main[0] = {data};
-            });
-    
-            //fips 44003
-            fetch('http://localhost:5000/county/44003', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }).then(response=>response.json())
-            .then(data => {
-                main[1] = {data};
-            });
-    
-            //fips 44005
-            fetch('http://localhost:5000/county/44005', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }).then(response=>response.json())
-            .then(data => {
-                main[2] = {data}
-            });
-    
-            //fips 44007
-            fetch('http://localhost:5000/county/44007', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }).then(response=>response.json())
-            .then(data => {
-                main[3] = {data}
-            });
-    
-            //fips 44009
-            fetch('http://localhost:5000/county/44009', {
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }).then(response=>response.json())
-            .then(data => {
-                main[4] = {data}
-                setLoading(true);
-            });
-    },[main])
+            }
+            setMain(data);
+            setLoading(true);
+        });
+    }, [counties]);
 
-    console.log(main)
     return (
         <div>
             {!loading ? <div>Loading....</div> : <div>
@@ -79,32 +51,21 @@ export const Informative = () => {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Fips</TableCell>
-                            <TableCell align="right">State</TableCell>
-                            <TableCell align="right">County&nbsp;</TableCell>
-                            <TableCell align="right">Total Cases&nbsp;</TableCell>
-                            <TableCell align="right">Total Deaths&nbsp;</TableCell>
-                            <TableCell align="right">Total Hospitalizations&nbsp;</TableCell>
-                            <TableCell align="right">Total Vaccinations&nbsp;</TableCell>
-                            <TableCell align="right">Last Updated&nbsp;</TableCell>
+                            {columns.map(column => (
+                                <TableCell align="right" key={column.name}>{column.name}</TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {main.map(county => (
                             <TableRow
-                            key={county.data.fips}
+                            key={`${county.fips}`}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell>
-                                    {county.data.fips}
-                                </TableCell>
-                                <TableCell align="right">{county.data.state_id === 44 ? <>Rhode Island</> : county.data.state_id}</TableCell>
-                                <TableCell align="right">{county.data.county_name}</TableCell>
-                                <TableCell align="right">{county.data.total_cases}</TableCell>
-                                <TableCell align="right">{county.data.total_deaths}</TableCell>
-                                <TableCell align="right">{county.data.total_hospital}</TableCell>
-                                <TableCell align="right">{county.data.total_vaccinated}</TableCell>
-                                <TableCell align="right">{county.data.last_updated_timestamp}</TableCell>
+                                {columns.map(column => (
+                                    <TableCell align="right" key={column.name}>{county[column.property]}</TableCell>
+                                ))}
+
                             </TableRow>
                         ))}
                     </TableBody>
@@ -116,4 +77,3 @@ export const Informative = () => {
 }
 
 export default Informative;
-
