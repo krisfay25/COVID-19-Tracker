1cac34ab793aa4d68b412b6722f1858b1fc7009b
kc <kaelachen22@gmail.com>
2021-11-15 18:01:00 -0500

    Move table for totals to its own component

--- MUI/src/pages/Informative.js
+++ MUI/src/pages/Informative.js
@@ -1,30 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import TemporaryDrawer from '../components/TemporaryDrawer';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import TotalsTable from '../components/TotalsTable';
 
 export const Informative = () => {
     const [loading, setLoading] = useState(false);
     const [main, setMain] = useState([]);
     // Fips codes for counties we currently support
-    const [counties] = useState([44001, 44003, 44005, 44009]);
-
-    // The columns of the table and what property in the data they correspond to
-    const columns = [
-        { name: 'Fips', property: 'fips' },
-        { name: 'State', property: 'state' },
-        { name: 'County', property: 'county_name' },
-        { name: 'Total Cases', property: 'total_cases' },
-        { name: 'Total Deaths', property: 'total_deaths' },
-        { name: 'Total Hospitalizations', property: 'total_hospital' },
-        { name: 'Total Vaccinations', property: 'total_vaccinated' },
-        { name: 'Last Updated', property: 'last_updated_timestamp' }
-    ];
+    const [counties] = useState([44001, 44003, 44005, 44007, 44009]);
 
     useEffect(() => {
         // Load county data
@@ -47,30 +29,7 @@ export const Informative = () => {
         <div>
             {!loading ? <div>Loading....</div> : <div>
             <TemporaryDrawer/>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map(column => (
-                                <TableCell align="right" key={column.name}>{column.name}</TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {main.map(county => (
-                            <TableRow
-                            key={`${county.fips}`}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                {columns.map(column => (
-                                    <TableCell align="right" key={column.name}>{county[column.property]}</TableCell>
-                                ))}
-
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            <TotalsTable data={main} />
             </div>}
         </div>
     )
