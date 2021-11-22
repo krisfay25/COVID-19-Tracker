2b97de327fbcc1dadf3ed03f68ba8267fda6279b
dpicolo1 <65634317+dpicolo1@users.noreply.github.com>
2021-11-22 17:00:06 -0500

    Fixed Typo in Death Rate attribute, Changed column width to fit names
    Fixed Typo in Death Rate attribute, Changed column width to fit names

--- MUI/src/components/MonthlyData.js
+++ MUI/src/components/MonthlyData.js
@@ -5,15 +5,15 @@ export const MonthlyData = ({data}) => {
 
   // Headers
   const columns = [
-    { headerName: 'Month', field: 'month' },
-    { headerName: 'Cases', field: 'num_cases' },
-    { headerName: 'Deaths', field: 'num_deaths' },
-    { headerName: 'Hospitalizations', field: 'num_hospit' },
-    { headerName: 'Vaccinations', field: 'num_vaxxed' },
-    { headerName: 'Case Rate', field: 'rate_cases' },
-    { headerName: 'Death Rate', field: 'rate_deahts' },
-    { headerName: 'Hospitalization Rate', field: 'rate_hospit' },
-    { headerName: 'Vaccination Rate', field: 'rate_vaxxed' }
+    { headerName: 'Month', field: 'month', width: 160 },
+    { headerName: 'Cases', field: 'num_cases', width: 160 },
+    { headerName: 'Deaths', field: 'num_deaths', width: 160 },
+    { headerName: 'Hospitalizations', field: 'num_hospit', width: 160 },
+    { headerName: 'Vaccinations', field: 'num_vaxxed', width: 160 },
+    { headerName: 'Case Rate', field: 'rate_cases', width: 160 },
+    { headerName: 'Death Rate', field: 'rate_deaths', width: 160 },
+    { headerName: 'Hospitalization Rate', field: 'rate_hospit', width: 160 },
+    { headerName: 'Vaccination Rate', field: 'rate_vaxxed', width: 160 }
   ]
 
   return (
--- MUI/src/components/TotalsTable.js
+++ MUI/src/components/TotalsTable.js
@@ -6,14 +6,14 @@ export const TotalsTable = ({data}) => {
     const rows: GridRowsProp = data;
 
     const columns: GridColDef[] = [
-        { headerName: 'Fips', field: 'fips' },
-        { headerName: 'State', field: 'state' },
-        { headerName: 'County', field: 'county_name' },
-        { headerName: 'Total Cases', field: 'total_cases' },
-        { headerName: 'Total Deaths', field: 'total_deaths' },
-        { headerName: 'Total Hospitalizations', field: 'total_hospital' },
-        { headerName: 'Total Vaccinations', field: 'total_vaccinated' },
-        { headerName: 'Last Updated', field: 'last_updated_timestamp' }
+        { headerName: 'Fips', field: 'fips', width: 160 },
+        { headerName: 'State', field: 'state', width: 160 },
+        { headerName: 'County', field: 'county_name', width: 160 },
+        { headerName: 'Total Cases', field: 'total_cases', width: 160 },
+        { headerName: 'Total Deaths', field: 'total_deaths', width: 160 },
+        { headerName: 'Total Hospitalizations', field: 'total_hospital', width: 170 },
+        { headerName: 'Total Vaccinations', field: 'total_vaccinated', width: 160 },
+        { headerName: 'Last Updated', field: 'last_updated_timestamp', width: 160 }
     ];
 
     return(
